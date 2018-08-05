110d469b89bf022548fd72b769c0c952e8a5e0d2
Taro Hoshino <no_taro1990@outlook.com>
2018-08-05 14:41:44 +0900

    整理

--- lib/isNumeric.bat
+++ lib/isNumeric.bat
@@ -1,5 +1,4 @@
 @echo off
-
 rem **
 rem * 数値かどうか判定する。第1引数を数値かどうか判定して、その結果を環境変数RETに代入する。
 rem * 数値である場合は1、数値でない場合は0が代入される。この処理の終了ステータスは常に0である。
@@ -8,7 +7,6 @@ rem * @param %1 数値かどうか判定する値
 rem * @return 数値である場合1、数値でない場合0
 rem * @exit 0 正常終了
 rem *
-
 setlocal
 set RET=0
 
@@ -18,5 +16,4 @@ if %ERRORLEVEL% equ 0 (
     set RET=1
 )
 endlocal && set RET=%RET%
-
 exit /b 0
